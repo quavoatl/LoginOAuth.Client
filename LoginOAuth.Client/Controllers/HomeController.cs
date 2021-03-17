d0888be0af9be41dd39ccba55b22ed91d72da216
Sergiu Alexandru Antal <sergiu.antal@endava.com>
2021-03-17 21:27:41 +0200

    Initial commit

---
+++ LoginOAuth.Client/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginOAuth.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Secret()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+           
+            var claims = HttpContext.User.Claims;
+
+            return View("Secret", accessToken);
+        }
+    }
+}
---
+++ LoginOAuth.Client/Startup.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+
+namespace LoginOAuth.Client
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(config =>
+                {
+                    config.DefaultAuthenticateScheme = "ClientCookie";
+                    config.DefaultScheme = "ClientCookie";
+                    config.DefaultChallengeScheme = "OurServer";
+                })
+                .AddCookie("ClientCookie")
+                .AddOAuth("OurServer", config =>
+                {
+                    config.ClientId = "client_id";
+                    config.ClientSecret = "client_secret";
+                    config.CallbackPath = "/oauth/callback";
+                    config.AuthorizationEndpoint = "https://localhost:5002/oauth/authorize";
+                    config.TokenEndpoint = "https://localhost:5002/oauth/token";
+                    config.SaveTokens = true;
+
+                    config.Events = new OAuthEvents()
+                    {
+                        OnCreatingTicket = context =>
+                        {
+                            var accessToken = context.AccessToken;
+                            var base64Payload = accessToken.Split('.')[1];
+                            var bytes = Convert.FromBase64String(base64Payload);
+                            var jsonPayload = Encoding.ASCII.GetString(bytes);
+                            var claims = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonPayload);
+
+                            foreach (var claim in claims)
+                            {
+                                context.Identity.AddClaim(new Claim(claim.Key, claim.Value));
+                            }
+
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+        }
+    }
+}
